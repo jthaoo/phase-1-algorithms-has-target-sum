9fa178ac881b1cbad5758ea84722992063edaaf2
Johnathan Thao <johnathanthao21@gmail.com>
2022-06-28 15:40:28 -0600

    commit tree 8f4a50a24679e83d7846fe414e4bff804a64f4b7

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 function hasTargetSum(array, target) {
-  
-  let sum = [];
+  const seenNumbers = {};
 
-  let hashTable = {};
-
-  for (let x = 0; x < array.length; x++) {
-      let sumMinus = target - array[x];
-
-      if (hashTable[sumMinus.toString()] !== undefined) {
-          sum.push([array[x]], sumMinus);
-          return true;
-
-
-      }
-      hashTable[array[x].toString()] = array[x];
+  for(const number of array) {
+    const complement = target - number;
+    if (complement in seenNumbers) return true
+    seenNumbers[number] = true;
   }
-  return false;
+
+  return false
+
 }
-hasTargetSum([3, 8, 12, 4, 11, 7], 10);
 
 if (require.main === module) {
   
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, -2, 4], 2));
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
